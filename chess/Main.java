cff2fb59d16c70d2e703f9386df94ea08d240e80
leonardcser <73912641+leonardcser@users.noreply.github.com>
2024-12-14 12:16:52 +0100

    Add Main class

---
+++ chess/Main.java
@@ -0,0 +1,12 @@
+package chess;
+
+import chess.engine.ChessEngine;
+import chess.views.gui.GUIView;
+
+public class Main {
+    public static void main(String[] args) {
+        ChessEngine chessController = new ChessEngine();
+        GUIView view = new GUIView(chessController);
+        view.startView();
+    }
+}
---
+++ chess/engine/ChessEngine.java
@@ -0,0 +1,22 @@
+package chess.engine;
+
+import chess.ChessController;
+import chess.ChessView;
+
+public class ChessEngine implements ChessController {
+
+    @Override
+    public void start(ChessView view) {
+        
+    }
+
+    @Override
+    public boolean move(int fromX, int fromY, int toX, int toY) {
+        return false;
+    }
+
+    @Override
+    public void newGame() {
+
+    }
+}
