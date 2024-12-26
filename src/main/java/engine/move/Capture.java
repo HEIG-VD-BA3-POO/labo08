b03d1699fbc938ff9a9b057a1e78ee884b85d6f8
leonardcser <73912641+leonardcser@users.noreply.github.com>
2024-12-26 19:46:54 +0100

    Refactor ChessMove + Pawn

--- src/main/java/engine/move/Capture.java
+++ src/main/java/engine/move/Capture.java
@@ -36,13 +36,7 @@ public class Capture extends ChessMove {
      */
     @Override
     public void execute(ChessBoard board) {
-        assert board.containsKey(from);
         assert board.containsKey(to);
-        board.setLastMove(this);
-        ChessPiece p = board.get(from);
-        board.remove(from);
-        board.remove(to);
-        p.markMoved();
-        board.put(to, p);
+        super.execute(board);
     }
 }
